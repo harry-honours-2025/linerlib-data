f950f5c86dd76814b200b0ebe6367e84863737c1
Harry Porter <h.porter23@icloud.com>
2025-06-18 22:50:57 +1000

    Begins cleaning up instance CSV files

---
+++ scripts/restructure_tab_separated_csvs.py
@@ -0,0 +1,21 @@
+import argparse
+import pandas
+import pathlib
+
+
+def restructure_csvs():
+    args = construct_and_parse_args()
+    if not args.parent_directory.is_absolute():
+        args.parent_directory = pathlib.Path.cwd() / args.parent_directory
+    for child_file in args.parent_directory.rglob("*.csv"):
+        pandas.read_csv(child_file, sep="\t").to_csv(child_file, index=False)
+
+
+def construct_and_parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("parent_directory", type=pathlib.Path)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    restructure_csvs()
