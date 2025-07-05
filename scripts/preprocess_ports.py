f92eb3500a1f154ed2c9e383f889beb7ed046c79
Harry Porter <h.porter23@icloud.com>
2025-07-05 14:30:38 +1000

    Refactored ports preprocessing logic

--- scripts/preprocess_ports.py
+++ scripts/preprocess_ports.py
@@ -1,23 +1,38 @@
+"""
+Script for preprocessing the LINERLIB ports CSV.
+Does not depend on the execution of any other scripts in advance.
+
+Discards all ports where both:
+    - The UNLOCODE does not appear as the origin or destination for any demand, and
+    - Any of the transshipment, fixed port call, or variable port call fields are null.
+"""
+
 import argparse
 import pandas
 import pathlib
 
 
-def preprocess_ports_and_distances():
+def preprocess_ports():
     args = construct_and_parse_args()
     args.output_directory.mkdir(parents=True, exist_ok=True)
     demands_data = pandas.concat(
         (pandas.read_csv(file) for file in args.demands_directory.rglob("*.csv")),
         ignore_index=True,
     )
-    port_codes = pandas.concat(
-        [
-            demands_data["Origin (UNLOCODE)"],
-            demands_data["Destination (UNLOCODE)"],
-        ]
-    )
+    port_codes = pandas.concat([
+        demands_data["Origin (UNLOCODE)"],
+        demands_data["Destination (UNLOCODE)"],
+    ])
     ports_data = pandas.read_csv(args.ports_csv_file)
-    ports_data = ports_data[ports_data["UNLOCODE"].isin(port_codes)]
+    cost_fields = [
+        "Transshipment Cost Per FFE (USD)",
+        "Fixed Port Call Cost (USD)",
+        "Variable Port Call Cost Per FFE (USD)",
+    ]
+    ports_data = ports_data[
+        ports_data["UNLOCODE"].isin(port_codes) |
+        ports_data[cost_fields].notna().all(axis=1)
+    ]
     ports_data.to_csv(args.output_directory / args.ports_csv_file.name, index=False)
 
 
@@ -34,4 +49,4 @@ def construct_and_parse_args():
 
 
 if __name__ == "__main__":
-    preprocess_ports_and_distances()
+    preprocess_ports()
